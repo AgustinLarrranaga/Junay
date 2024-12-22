d901de0c58b81aba435dc4cd75418e20fec52110
unknown <agustinlarra99@gmail.com>
2024-12-22 20:46:21 -0300

    home y header

--- junay/src/app/app.component.ts
+++ junay/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MapaComponent } from './mapa/mapa.component';
 import { WhatsappButtonComponent } from './whatsapp-button/whatsapp-button.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MapaComponent, WhatsappButtonComponent],
+  imports: [MapaComponent, WhatsappButtonComponent,WelcomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ junay/src/app/welcome/welcome.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+
+@Component({
+    selector: 'app-welcome',
+    standalone: true,
+    templateUrl: './welcome.component.html',
+    styleUrls: ['./welcome.component.css'],
+})
+export class WelcomeComponent {
+}
+
