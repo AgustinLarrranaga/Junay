cc6345083f318f1c818a47cf34f96a8da450f63b
unknown <agustinlarra99@gmail.com>
2024-12-28 20:10:51 -0300

    more

--- junay/src/app/contact/contact.component.ts
+++ junay/src/app/contact/contact.component.ts
@@ -15,4 +15,48 @@ export class ContactComponent {
     onSubmit() {
         alert('Formulario enviado correctamente');
       }
+
+
+      redirectToGoogleMaps(): void {
+        navigator.geolocation.getCurrentPosition((position) => {
+          const userLat = position.coords.latitude;
+          const userLng = position.coords.longitude;
+    
+          // Coordenadas o dirección del destino
+          const destination = 'San Salvador 9758, B7600GMJ Mar del Plata, Provincia de Buenos Aires'; // Ejemplo con dirección
+          // const destination = '-34.603684, -58.381559'; // Ejemplo con coordenadas
+    
+          const mapsUrl = `https://www.google.com/maps/dir/?api=1&origin=${userLat},${userLng}&destination=${encodeURIComponent(destination)}`;
+    
+          // Abrir en una nueva pestaña
+          window.open(mapsUrl, '_blank');
+        },
+        (error) => {
+          console.error('Error al obtener la ubicación:', error);
+          alert('No se pudo obtener tu ubicación. Verifica los permisos.');
+        });
+      }
+
+
+      redirectToWhatsApp(): void {
+        const phoneNumber = '+5492234475381'; 
+        const message = 'Hola vengo de la pagina web'; 
+        const encodedMessage = encodeURIComponent(message);
+        
+        const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
+    
+        window.open(whatsappUrl, '_blank');
+      }
+
+      sendEmail(): void {
+        const email = 'destinatario@example.com'; // Dirección del destinatario
+        const subject = 'Consulta desde mi aplicación'; // Asunto del correo
+        const body = 'Hola,\n\nQuería consultarte sobre lo siguiente:\n\nSaludos.'; // Cuerpo del correo
+    
+        // Construcción del enlace mailto
+        const mailtoLink = `mailto:${this.mail}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+        console.log(mailtoLink)
+        // Redirigir al enlace mailto
+        window.location.href = mailtoLink;
+      }
 }
--- junay/src/app/footer/footer.component.ts
+++ junay/src/app/footer/footer.component.ts
@@ -10,4 +10,25 @@ import { FormsModule } from '@angular/forms';
 export class FooterComponent {
 
     
+    redirectToWhatsApp(): void {
+        const phoneNumber = '+5492234475381'; 
+        const message = 'Hola vengo de la pagina web'; 
+        const encodedMessage = encodeURIComponent(message);
+        
+        const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
+    
+        window.open(whatsappUrl, '_blank');
+      }
+
+      openInstagram(): void {
+        const instagramUrl = 'https://www.instagram.com/distribuidorajunay/';
+        window.open(instagramUrl, '_blank'); // Abrir en una nueva pestaña
+      }
+
+      openFacebook(): void {
+        const FacebookUrl = 'https://www.facebook.com/profile.php?id=61570521983692';
+        window.open(FacebookUrl, '_blank'); // Abrir en una nueva pestaña
+      }
+
+      
 }
